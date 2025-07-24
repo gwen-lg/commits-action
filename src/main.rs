8873dc4cb46fbcfdfd5df64aa714bd1c7b175482
Gwen Lg <me@gwenlg.fr>
2025-07-24 22:00:45 +0200

    feat: remove error management (example)
    - remove `error` input
    - remove `error` input management from rust code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use std::{env, fs::write, process::exit};
-
 fn main() {
-    let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
-
-    let args: Vec<String> = env::args().collect();
-    let error = &args[1];
-
-    if !error.is_empty() {
-        eprintln!("Error: {error}");
-        write(github_output_path, format!("error={error}")).unwrap();
-        exit(1);
-    }
+    eprintln!("::warning title=empty action::This action do nothing");
 }
