7dee9a163a17c375ae76f9b0ec99ab27fe853f07
Gwen Lg <me@gwenlg.fr>
2025-07-24 23:11:34 +0200

    wip: add debug management

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,13 @@ enum Error {
 
 fn main() -> anyhow::Result<()> {
     let _args = Cli::parse();
+    let debug = env::var("ACTIONS_RUNNER_DEBUG").is_ok();
     let github_event_name =
         env::var("GITHUB_EVENT_NAME").map_err(Error::MissingGithubEventNameEnvVar)?;
 
-    eprintln!("::warning title=empty action::This action have been triggered by `{github_event_name}` but do nothing");
+    if debug {
+        eprintln!("::warning title=empty action::This action have been triggered by `{github_event_name}` but do nothing");
+    }
 
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
 
