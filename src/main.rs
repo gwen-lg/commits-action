2af7d67d42244a13d76218b99dd48977294c99d2
Gwen Lg <me@gwenlg.fr>
2025-07-24 22:58:26 +0200

    wip: fixup github_event_name management

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use clap::Parser;
 use std::{env, fs::write};
+use thiserror::Error;
 
 #[derive(Parser)]
 #[command(version)]
 struct Cli {}
 
-fn main() {
+#[derive(Debug, Error)]
+enum Error {
+    #[error("The env var `GITHUB_EVENT_NAME` is missing.")]
+    MissingGithubEventNameEnvVar(#[source] env::VarError),
+}
+
+fn main() -> anyhow::Result<()> {
     let _args = Cli::parse();
+    let github_event_name =
+        env::var("GITHUB_EVENT_NAME").map_err(Error::MissingGithubEventNameEnvVar)?;
 
-    let github_event = env::var("GITHUB_EVENT").unwrap();
-
-    eprintln!("::warning title=empty action::This action have been triggered by `{github_event}` but do nothing");
+    eprintln!("::warning title=empty action::This action have been triggered by `{github_event_name}` but do nothing");
 
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
 
     let commits = "wip"; //TODO
     write(github_output_path, format!("commits={commits}")).unwrap();
+
+    Ok(())
 }
