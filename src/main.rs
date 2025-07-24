c73ecc1fdf63cbf860e0b11f1ea3148ba78f11eb
Gwen Lg <me@gwenlg.fr>
2025-07-24 22:36:11 +0200

    feat: add `--version` management with clap
    add clap dependency with `derive` feature

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use clap::Parser;
 use std::env;
 
+#[derive(Parser)]
+#[command(version)]
+struct Cli {}
+
 fn main() {
+    let _args = Cli::parse();
+
     let github_event = env::var("GITHUB_EVENT").unwrap();
 
     eprintln!("::warning title=empty action::This action have been triggered by `{github_event}` but do nothing");
